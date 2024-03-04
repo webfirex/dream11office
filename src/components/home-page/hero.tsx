e5818b50d11d7f2a44fc9439d35b515ccc8dc4e7
Prakhar Vohra <webfirex@gmail.com>
2024-03-04 20:19:16 +0530

    Updates

--- src/components/home-page/hero.tsx
+++ src/components/home-page/hero.tsx
@@ -1,8 +1,11 @@
-import { AspectRatio, Image } from "@mantine/core";
-import { Data } from "~/lib/data";
+import { Image } from "@mantine/core";
+import Link from "next/link";
+// import { Data } from "~/lib/data";
 
 export const HomeHeroComp = () => {
   return (
-    <a href="/view/41" className="w-full"><Image src={'https://imagetolink.com/ib/dQRbpYmmZu.png'} className={'w-full'} /></a>
+    <Link href={'/view/41'} className="w-full">
+      <Image src={'https://imagetolink.com/ib/dQRbpYmmZu.png'} className={'w-full'} />
+    </Link>
   )
 };
