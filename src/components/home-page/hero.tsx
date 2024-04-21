a8ff19a7cb2bb07afb9142ebe6fb273fc4afef58
Prakhar Vohra <webfirex@gmail.com>
2024-04-21 20:35:52 +0530

    Updates

--- src/components/home-page/hero.tsx
+++ src/components/home-page/hero.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 
 export const HomeHeroComp = () => {
   return (
-    <Link href={'/view/64'} className="w-full">
-      <Image src={'https://imagetolink.com/ib/blYKDvCzQi.png'} className={'w-full'} />
+    <Link href={'/view/71'} className="w-full">
+      <Image src={'https://imagetolink.com/ib/9oVEmTjkFi.png'} className={'w-full'} />
     </Link>
   )
 };
