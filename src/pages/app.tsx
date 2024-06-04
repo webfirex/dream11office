8519dbd6069d24fde0fd430ddf7e0a9675f38e95
Prakhar Vohra <webfirex@gmail.com>
2024-06-04 22:35:26 +0530

    Updates

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -131,11 +131,11 @@ export default function App({
         <Stack p="md">
           
           <Modal opened={opened} withCloseButton={false} onClose={close} centered radius={'md'}>
-            <Link href={'/view/79'} style={{
+            <Link href={'/view/76'} style={{
               border: 'none',
               borderRadius: '20px'
             }}>
-              <Image src={'https://imagetolink.com/ib/6uyiRsmNLv' + '.png'} w={'100%'} radius={'md'} />
+              <Image src={'https://imagetolink.com/ib/a0gBoxba2t' + '.png'} w={'100%'} radius={'md'} />
             </Link>
           </Modal>
 
