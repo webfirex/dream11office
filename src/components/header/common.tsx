68f9dc0a8f002b0528ff626fed3b7428dc5cf006
Prakhar Vohra <webfirex@gmail.com>
2024-10-20 21:25:41 +0530

    Updates

--- src/components/header/common.tsx
+++ src/components/header/common.tsx
@@ -4,7 +4,7 @@ export const CommonHeader = () => {
   return (
     <>
       <Group h="100%" px="md" bg="#a50c0c" display={"flex"} justify={"space-between"}>
-        <Image src="h-logo-w.png" alt="Header Logo" h={50} />
+        <Image src="/h-logo-w.png" alt="Header Logo" h={50} />
         <Title order={4} c={"#fff"}>Dream 11 Office</Title>
       </Group>
     </>
