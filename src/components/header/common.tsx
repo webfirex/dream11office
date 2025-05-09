6c9473819d054bc4ca29c8eb1497a57274cc5823
Prakhar Vohra <webfirex@gmail.com>
2025-05-09 19:49:36 +0530

    Updates

--- src/components/header/common.tsx
+++ src/components/header/common.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Button, Group, Image, Title } from "@mantine/core";
+import { Button, Divider, Group, Image, Title } from "@mantine/core";
 import { useEffect, useState } from "react";
 import Cookies from 'js-cookie';
 
@@ -80,21 +80,22 @@ export const CommonHeader = () => {
 
   return (
     <>
-      <Group h="100%" px="xs" bg="#fff" display={"flex"} justify={"space-between"} style={{ boxShadow: '0 0 10px #00000050', borderTop: '1px', borderColor: '#00000050', flexWrap: 'nowrap' }}>
-        <Image src="/h-logo-w.png" alt="Header Logo" h={50} />
-        <div style={{display: 'flex', gap: '5px', alignItems: 'center', justifyContent: 'end', height: '100%'}}>
-          <Image src="/contactus.png" alt="Header Logo" h={40} />
-          <Image src={ !sidebar ? profPic : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGO8tu34AXoBHkvfWOS8WTAvtf42V2tHomBw&s" } alt="Header Logo" h={40} w={40} radius={'100%'} onClick={() => {sidebar ? setSidebar(false) : setSidebar(true)}} style={{cursor: 'pointer'}} />
+      <Group h="100%" px="xs" bg="#a50c0c" display={"flex"} justify={"space-between"} style={{ boxShadow: '0 0 10px #00000050', borderTop: '1px', borderColor: '#00000050', flexWrap: 'nowrap' }}>
+        <div style={{display: 'flex', gap: '5px', alignItems: 'center', justifyContent: 'start', height: '100%'}}>
+          <Image src={ !sidebar ? profPic : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGO8tu34AXoBHkvfWOS8WTAvtf42V2tHomBw&s" } alt="Header Logo" h={35} w={35} radius={'100%'} onClick={() => {sidebar ? setSidebar(false) : setSidebar(true)}} style={{cursor: 'pointer'}} />
+          <Title order={4} c={"#fff"}>Head Office</Title>
         </div>
-        {/* <Title order={4} c={"#F40000"}>Dream 11</Title> */}
+        <Image src="/contactus.png" alt="Header Logo" h={40} />
       </Group>
-      {sidebar && <div style={{display: 'flex', position: 'fixed', backgroundColor: '#fff', right: '0', padding: '14px', height: '100%', boxShadow: '0 10px 10px #00000050', width: '70%', flexDirection: 'column', gap: '14px' }}>
-        <h1 style={{margin: '0', fontSize: '22px'}}>Hi, {name}</h1>
+      {sidebar && <div style={{display: 'flex', position: 'fixed', backgroundColor: '#fff', left: '0', padding: '14px', height: '100%', boxShadow: '0 10px 10px #00000050', width: '70%', flexDirection: 'column', gap: '14px' }}>
+        <Image src="/h-logo-w.png" alt="Header Logo" w={125} ml={-10} mt={-15} mb={-15} style={{alignSelf: 'start'}} />
+        <Divider variant="solid" content="or" c={"dark"} w={'100%'} my={'5px'} labelPosition="center" />
+        <h1 style={{margin: '0', fontSize: '20px'}}>Hi, {name}</h1>
         <div style={{display: 'flex', gap: '10px', justifyContent: 'start'}}>
-          <Image src={profPic} alt="Header Logo" h={50} w={50} radius={'100%'} style={{cursor: 'pointer'}} onClick={() => handleSelectImage()} />
+          <Image src={profPic} alt="Header Logo" h={45} w={45} radius={'100%'} style={{cursor: 'pointer'}} onClick={() => handleSelectImage()} />
           <div style={{display: 'flex', gap: '5px', justifyContent: 'start', flexDirection: 'column'}}>
-            <p style={{ margin: '0', fontSize: '14px'}}>Name: {name}</p>
-            <p style={{ margin: '0', fontSize: '14px'}}>Phone: +91 9999999999</p>
+            <p style={{ margin: '0', fontSize: '12px'}}>Name: {name}</p>
+            <p style={{ margin: '0', fontSize: '12px'}}>Phone: +91{phone}</p>
           </div>
         </div>
         <Button variant="filled" color="red">Logout</Button>
